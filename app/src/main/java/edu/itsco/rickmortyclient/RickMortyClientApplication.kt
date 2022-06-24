4b20a94298fa252b6882cbd5bbfa82e393c78600
AxelLpz <axelmujica8687@gmail.com>
2022-06-24 14:34:39 -0500

    Initial commit

---
+++ app/src/main/java/edu/itsco/rickmortyclient/RickMortyClientApplication.kt
@@ -0,0 +1,9 @@
+package edu.itsco.rickmortyclient
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class RickMortyClientApplication: Application() {
+}
